05e6b6d14be0e018c4b7c50ada2d2c98d2a53c0c
desmond1999d <desmond1999d@mail.ru>
2018-11-27 01:19:16 +0300

    test

--- be/src/main/java/by/training/nc/sd3/service/impl/BillingAccountServiceImpl.java
+++ be/src/main/java/by/training/nc/sd3/service/impl/BillingAccountServiceImpl.java
@@ -9,6 +9,7 @@ import by.training.nc.sd3.service.SubscriptionUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 import java.util.Optional;
 
 @Component
